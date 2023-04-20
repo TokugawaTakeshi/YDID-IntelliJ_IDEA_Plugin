9a5b8929e5804fb9400abe72fee9fa7d3863d3de
GitHub Action <action@github.com>
2023-04-20 08:40:50 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "YDID-IntelliJ_IDEA_Plugin"
--- src/main/kotlin/com/github/tokugawatakeshi/ydidintellijideaplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/tokugawatakeshi/ydidintellijideaplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tokugawatakeshi.ydidintellijideaplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
